0897c0031e64085289a1fa2e3df96cdee0fb3ae0
logan <gengjiarong@xiaoice.com>
2023-12-22 23:11:28 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'hello You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
