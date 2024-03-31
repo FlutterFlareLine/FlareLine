38af73f0456a63f9d82d3e8c77260dc23ee111d9
logan <gengjiarong@xiaoice.com>
2024-03-31 12:22:19 +0800

    fix run on macos

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,38 @@
-import 'package:bitsdojo_window/bitsdojo_window.dart';
-import 'package:flareline/themes/global_theme.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
-import 'package:flutter/material.dart';
 import 'package:flareline/provider/localization_provider.dart';
 import 'package:flareline/provider/main_provider.dart';
 import 'package:flareline/routes.dart';
+import 'package:flareline/themes/global_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
 
   await GetStorage.init();
 
   usePathUrlStrategy();
 
-  runApp(MyApp());
-
-  doWhenWindowReady(() {
-    appWindow.minSize = const Size(480, 360);
-    appWindow.size = Size.infinite;
-    appWindow.alignment = Alignment.center;
-    appWindow.show();
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(1080, 720),
+    minimumSize: Size(480, 360),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
   });
+
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import bitsdojo_window_macos
 import path_provider_foundation
+import screen_retriever
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
