d789d84968fd8ee091b895a6a9fec17d56d1baf2
spiky-star <spikystar.dev@gmail.com>
2024-04-18 01:40:34 +0000

    update

--- lib/pages/scrapy/scrapy_provider.dart
+++ lib/pages/scrapy/scrapy_provider.dart
@@ -31,9 +31,14 @@ class ScrapyProvider extends ChangeNotifier {
 
     _isLoading = true;
     notifyListeners();
-
+String userAgentFallback =
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
+  Map<String, String>? allHeaders = {
+  
+    'User-Agent': userAgentFallback
+  };
     try {
-      final response = await http.get(Uri.parse(controller.text));
+      final response = await http.get(Uri.parse(controller.text),headers:allHeaders);
       if (response.statusCode == 200) {
         _text = response.body;
       } else {
