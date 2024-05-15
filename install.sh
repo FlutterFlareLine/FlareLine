f3e6c341c7b938b01892e836a874608c0e609d6b
logan <gengjiarong@xiaoice.com>
2024-05-15 13:52:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 FLUTTER_VERSION="3.19.3"
 
-if cd flutter; then git pull && cd .. ; else git clone https://github.com/flutter/flutter.git; fi && ls && flutter/bin/flutter doctor && flutter/bin/flutter clean && flutter/bin/flutter config --enable-web
+if cd flutter; then git pull && cd .. ; else git clone -b $FLUTTER_VERSION https://github.com/flutter/flutter.git; fi && ls && flutter/bin/flutter doctor && flutter/bin/flutter clean && flutter/bin/flutter config --enable-web
 
 #flutter/bin/flutter pub get
 
