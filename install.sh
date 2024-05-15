6de1e67207e8888aa72e0391e6b8635363bb06fa
logan <gengjiarong@xiaoice.com>
2024-05-15 13:50:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,10 @@
 
 FLUTTER_VERSION="3.19.3"
 
-if [ -d "flutter" ]; then
-    echo "Flutter already installed"
-else
-    git clone -b $FLUTTER_VERSION https://github.com/flutter/flutter.git
-fi
+if cd flutter; then git pull && cd .. ; else git clone https://github.com/flutter/flutter.git; fi && ls && flutter/bin/flutter doctor && flutter/bin/flutter clean && flutter/bin/flutter config --enable-web
 
-ls
-flutter/bin/flutter --version
-#flutter/bin/flutter config --enable-web
-flutter/bin/flutter pub get
+#flutter/bin/flutter pub get
+
 flutter/bin/flutter gen-l10n
+
 #flutter/bin/flutter build web --release --web-renderer html
