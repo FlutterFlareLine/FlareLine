f17c3d995ebce194519feb365d7364fe0463bbd0
logan <gengjiarong@xiaoice.com>
2024-05-15 13:37:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ fi
 
 ls
 flutter/bin/flutter --version
-flutter/bin/flutter config --enable-web
+#flutter/bin/flutter config --enable-web
 flutter/bin/flutter pub get
 flutter/bin/flutter gen-l10n
 #flutter/bin/flutter build web --release --web-renderer html
